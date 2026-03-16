05444085058dd88d42e6b5a7e3efd07495ec3c47
zmyke05 <michele.papa2005@gmail.com>
2026-03-16 12:04:53 +0100

    commit fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Main } from './layout/main/main';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -11,4 +12,5 @@ export const routes: Routes = [
     children: [{ path: 'home', component: Home }],
   },
   { path: 'sign-in', component: Login },
+  { path: 'sign-up', component: Register },
 ];
