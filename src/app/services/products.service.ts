b9ef9972217b0d0de4a60eb0b6fbce6f29f39627
Rizal <gabrieledigrazia@protonmail.com>
2026-03-16 21:29:28 +0100

    feat: implement basic login

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,15 +1,14 @@
 import { Injectable, resource } from '@angular/core';
 import { Product } from '../components/shared/product-card/product-card';
+import { baseApiUrl } from '../constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  private baseUrl = 'http://localhost:8080/api';
-
   products = resource<Product[], string>({
     loader: async () => {
-      const response = await fetch(`${this.baseUrl}/products`);
+      const response = await fetch(`${baseApiUrl}/products`);
 
       if (!response.ok) {
         throw new Error('Non è stato possibile caricare i prodotti.');
