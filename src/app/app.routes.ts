7fec6f15d62bed03996745b82cc3dcf174e9f128
zmyke05 <michele.papa2005@gmail.com>
2026-03-16 16:10:14 +0100

    add: routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,17 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Main } from './layout/main/main';
 import { Register } from './pages/register/register';
+import { Category } from './pages/category/category';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: '',
     component: Main,
-    children: [{ path: 'home', component: Home }],
+    children: [
+      { path: 'home', component: Home },
+      { path: 'category', component: Category },
+    ],
   },
   { path: 'sign-in', component: Login },
   { path: 'sign-up', component: Register },
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProductCard } from "../../components/shared/product-card/product-card";
 import { ChiSiamo } from "../../components/shared/chi-siamo/chi-siamo";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [ProductCard, ChiSiamo],
+  imports: [ProductCard, ChiSiamo, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
